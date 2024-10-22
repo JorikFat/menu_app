6bf10d08cae31ee4de64375ad1a9c2a8e3008d9c
JorikFat <jorikfat@gmail.com>
2024-10-22 21:04:27 +0300

    add: bottom bar

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,14 +12,40 @@ class HomeScreen extends StatefulWidget {
 
 class _State extends State<HomeScreen> {
   final CatalogBloc catalog = CatalogBloc();
-
+  int _pageIndex = 0;
+  late final List<Widget> pages = [
+    CatalogWidget(catalog),
+    CatalogWidget(catalog)
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text(APP_NAME),),
+      appBar: AppBar(
+        title: const Text(APP_NAME),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
       body: CatalogWidget(catalog),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _pageIndex,
+        onTap: _changePage,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list_alt),
+            label: "Каталог",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: "Корзина",
+            
+          )
+        ],
+      ),
     );
   }
 
+  void _changePage(int index) {
+    _pageIndex = index;
+    setState(() {});
+  }
 }
