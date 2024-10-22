9796b0badb414b835d214af13fe17d829a733b23
JorikFat <jorikfat@gmail.com>
2024-10-23 01:01:00 +0300

    change: show catalog count by badge

--- lib/features/catalog/catalog_widget.dart
+++ lib/features/catalog/catalog_widget.dart
@@ -4,6 +4,7 @@ import 'package:menu_app/features/cart/cart_product.dart';
 import 'package:menu_app/features/catalog/catalog_bloc.dart';
 import 'package:menu_app/features/catalog/catalog_event.dart';
 import 'package:menu_app/features/catalog/catalog_state.dart';
+import 'package:menu_app/features/product.dart';
 
 class CatalogWidget extends StatelessWidget {
   final CatalogBloc bloc;
@@ -14,36 +15,44 @@ class CatalogWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<CatalogBloc, CatalogState>(
       bloc: bloc,
-      builder: (context, state) => switch(state){
+      builder: (context, state) => switch (state) {
         CatalogLoadState() => const _Load(),
         CatalogDataState() => ListView.separated(
-          padding: const EdgeInsets.all(16),
-          itemCount: state.products.length,
-          separatorBuilder: (_, __) => const SizedBox(height: 8),
-          itemBuilder: (context, index) => InkWell(
-            onTap: () => bloc.add(CatalogAddProductEvent(state.products[index])),
-            child: _Product(state.products[index]),
+            padding: const EdgeInsets.all(16),
+            itemCount: state.products.length,
+            separatorBuilder: (_, __) => const SizedBox(height: 8),
+            itemBuilder: (context, index) {
+              final CartProduct product = state.products[index];
+              final _Product widget = _Product(product);
+              return InkWell(
+                onTap: () => bloc.add(CatalogAddProductEvent(product)),
+                child: product.count == 0
+                    ? widget
+                    : Badge(
+                        label: Text(product.count.toString()),
+                        child: widget,
+                      ),
+              );
+            },
           ),
-        ),
-      }
+      },
     );
-  
   }
 }
 
 class _Load extends StatelessWidget {
-
   const _Load();
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: CircularProgressIndicator(),);
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
   }
-
 }
 
 class _Product extends StatelessWidget {
-  final CartProduct product;
+  final Product product;
 
   const _Product(this.product, {super.key});
 
@@ -56,11 +65,6 @@ class _Product extends StatelessWidget {
           children: [
             Expanded(child: Text(product.name)),
             Text(product.price.toString()),
-            if (product.count > 0)
-              Padding(
-                padding: const EdgeInsets.only(left: 8),
-                child: Text(product.count.toString()),
-              ),
           ],
         ),
       ),
