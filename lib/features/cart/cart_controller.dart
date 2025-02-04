ce6966aadf3c539e1149ac310b5fe8f58f2fa984
JorikFat <jorikfat@gmail.com>
2025-02-04 07:24:36 +0300

    test: cart controller

--- lib/features/cart/cart_controller.dart
+++ lib/features/cart/cart_controller.dart
@@ -3,20 +3,26 @@ import 'package:menu_app/features/product.dart';
 class CartController {
   final Map<Product, int> _productsCount = {};
 
-  int get count => _productsCount.values.reduce((value, element) => value + element);
+  int get count {
+    try {
+      return _productsCount.values.reduce((value, element) => value + element);
+    } on StateError {
+      return 0;
+    }
+  }
 
-  Map<Product, int> get state => Map.unmodifiable(_productsCount); 
+  Map<Product, int> get state => Map.unmodifiable(_productsCount);
 
   int countOf(Product product) => _productsCount[product] ?? 0;
 
-  void addProduct(Product product){
+  void addProduct(Product product) {
     final int count = countOf(product);
     _productsCount[product] = count + 1;
   }
 
-  void removeProduct(Product product){
+  void removeProduct(Product product) {
     final int count = _productsCount[product]!;
-    if(count == 1){
+    if (count == 1) {
       _productsCount.remove(product);
     } else {
       _productsCount[product] = count - 1;
