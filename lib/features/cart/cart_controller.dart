57b5161230b1788113a9c041f1e3d8eebc7af172
JorikFat <jorikfat@gmail.com>
2025-02-04 16:41:06 +0300

    refactor: cart controller count

--- lib/features/cart/cart_controller.dart
+++ lib/features/cart/cart_controller.dart
@@ -3,13 +3,9 @@ import 'package:menu_app/features/product.dart';
 class CartController {
   final Map<Product, int> _productsCount = {};
 
-  int get count {
-    try {
-      return _productsCount.values.reduce((value, element) => value + element);
-    } on StateError {
-      return 0;
-    }
-  }
+  int get count => _productsCount.isEmpty
+      ? 0
+      : _productsCount.values.reduce((value, element) => value + element);
 
   Map<Product, int> get state => Map.unmodifiable(_productsCount);
 
